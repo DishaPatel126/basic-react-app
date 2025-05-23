aeebd93f0ec8fc06356c9c787d28cdf385a9259a
DishaPatel126 <disha126p@gmail.com>
2025-05-22 21:32:57 -0300

    minor css changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,6 +12,10 @@ function App() {
     <div className="App">
       <h1>Hello World!</h1>
       <h2>{hello()}</h2>
+
+      <a href="https://github.com/DishaPatel126/basic-react-app/tree/main/react-app" target="_blank" rel="noopener noreferrer">
+        <button>Visit My GitHub Repo  </button>
+      </a>
     </div>
   );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//JSX element
 const myFirstElement = <h1>Hello World from Index.js!</h1>
 
 
